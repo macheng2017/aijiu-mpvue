0546af36c54e07c519c5e24b07b2c0020e74a7e6
macheng2017 <gos360@163.com>
2018-06-21 18:42:20 +0800

    init2

---
+++ server/config.local.js
@@ -0,0 +1,18 @@
+module.exports = {
+    mysql: {
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        pass: 'root',
+        db: 'cAuth',
+        char: 'utf8mb4'
+    },
+    serverHost: 'localhost',
+    tunnelServerUrl: '',
+    tunnelSignatureKey: '',
+    // 腾讯云相关配置可以查看云 API 秘钥控制台：https://console.qcloud.com/capi
+    qcloudAppId: '1255979890',
+    qcloudSecretId: 'AKIDoY9ZyGGOBoHblrOaEh1CBzb3KPJG3kAT',
+    qcloudSecretKey: 'JVWN4d2FHDyXv260jIj9QWSDuRp2ELLE',
+    wxMessageToken: ''
+}
