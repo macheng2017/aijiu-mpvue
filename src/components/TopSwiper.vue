279cd2bde4866c1cb7e8d2d9c386ffabe1e0d153
macheng2017 <gos360@163.com>
2018-07-24 10:19:26 +0800

    添加weui,添加搜索,swiper

--- src/components/TopSwiper.vue
+++ src/components/TopSwiper.vue
@@ -1,19 +1,47 @@
 <template>
-  <div>
-    <p class="card">
-      {{text}}
-    </p>
+  <div class="page">
+    <div class="page__bd page__bd_spacing">
+      <swiper :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration" :circular="circular" @change="swiperChange" @animationfinish="animationfinish">
+        <div v-for="item in imgUrls" :key="index">
+          <swiper-item>
+            <image :src="item" class="slide-image" />
+          </swiper-item>
+        </div>
+      </swiper>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['text']
+  data() {
+    return {
+      indicatorDots: true,
+      autoplay: true,
+      interval: 5000,
+      duration: 900,
+      circular: true,
+      imgUrls: [
+        'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      ]
+    }
+  },
+  methods: {
+    swiperChange(e) {
+      console.log('第' + e.mp.detail.current + '张轮播图发生了滑动')
+    },
+    animationfinish(e) {
+      console.log('第' + e.mp.detail.current + '张轮播图滑动结束')
+    }
+  }
 }
 </script>
 
 <style>
-.card {
-  padding: 10px;
+.slide-image {
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import store from '@/store'
+import '../static/css/weui.css'
 
 Vue.config.productionTip = false
 App.mpType = 'app'
