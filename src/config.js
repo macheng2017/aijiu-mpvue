04a6f70e3c0e434ac6d1f3a7f9023a4920a3ed76
mac <mac@macs-iMac.local>
2018-10-15 00:46:49 +0800

    切换到https

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 // 配置项
 // const host = 'http://localhost:5757'
 // 从本地切换为线上开发环境域名
-const host = 'http://mina.guolinaijiu.com'
+const host = 'https://mina.guolinaijiu.com'
 
 const config = {
   host,
   loginUrl: `${host}/weapp/login`,
-  articleUrl: 'http://www.guolinaijiu.com',
+  articleUrl: 'https://www.guolinaijiu.com',
   imageCDN: 'http://mini.guolinaijiu.com/'
 }
 export default config
