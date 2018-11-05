b90ad38261b10711c51dac4e462be8032606c169
mac <mac@macs-iMac.local>
2018-11-05 09:15:06 +0800

    修改轮播图,从public文件夹中获取静态图片

--- src/components/TopSwiper.vue
+++ src/components/TopSwiper.vue
@@ -22,9 +22,8 @@ export default {
       duration: 900,
       circular: true,
       imgUrls: [
-        'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-        'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-        'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+        '../../public/img/banner1.jpg',
+        '../../public/img/banner2.jpg'
       ]
     }
   },
